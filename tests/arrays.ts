33ae034e9a0bcceba400760d0bb5d5170d2b29f2
cryptosbyte <zakim019.211@spwtstudent.net>
2021-09-10 19:47:01 +0100

    🐝 "Tests" Folder included

---
+++ tests/arrays.ts
@@ -0,0 +1,25 @@
+import * as Arrays from "../lib/arrays";
+
+/*
+    Testing RandomIndex method to get a random index from an array.
+    // console.log(Arrays.RandomIndex(["some", "cool", "project", "poggers"]));
+*/
+
+/* 
+    Testing FitlerExact method to get the total amount of exact appearances of 1 inside of an array.
+    // console.log(Arrays.FilterExact([1, 6, 2, 1, 1, 1], 1));
+*/
+
+/*
+    Testing LoopFunction method to append a string with "a" 4 times.
+    // let Str = "";
+    // Arrays.LoopFunction(4, () => (Str += "a"));
+
+    // console.log(Str);
+*/
+
+
+/*
+    Testing ArrayToObject method to convert an array to an object
+    // console.log(Arrays.ArrayToObject(["test", "b", 5]))
+*/
---
+++ tests/objects.ts
@@ -0,0 +1,12 @@
+import * as Objects from "../lib/objects";
+
+/*
+    Testing ReverseObjectKeyValue method to convert the given object's value as the key and the original key as the value.
+    // console.log(Objects.ReverseObjectKeyValue({"cool":"bye", 12:12, "lol": 62, 1: "lol"}))
+*/
+
+/*
+    Testing GetValueFromPath method to get the value of the path given (converts into a dot notation path to check if it's inside of the object)
+    // let myObj = { nice: 1, poggers: { test: "ok", yep: true } }
+    // console.log(Objects.GetValueFromPath(myObj, "poggers.test"))
+*/
